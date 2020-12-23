c3cbd7c2c2424433d581552ef40433f74e1b50ed
liurushen <704790860@qq.com>
2020-12-23 22:20:28 +0800

    zz

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
 <template>
-  <router-view></router-view>
+  <header>
+    <van-nav-bar title="发现" />
+  </header>
+  <main>
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { Toast } from 'vant';
 
-export default defineComponent ({
-  name: 'App'
+export default  ({
+  
+//  data() {
+ 
+//  },
+ setup() {
+   
+    },
+   
+  methods: {
+}
 })
 </script>
+<style lang="less">
+.my-swipe .van-swipe-item {
+    color: #fff;
+    font-size: 20px;
+    line-height: 150px;
+    text-align: center;
+    background-color: #39a9ed;
+  }
+.van-swipe{
+  
+  border-radius:6px !important ;
+}
+</style>
