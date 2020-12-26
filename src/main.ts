5928e867ce3c30fe4a26ca79b3d0d7823195659b
zxz111222 <1093479419@qq.com>
2020-12-26 09:30:42 +0800

    zhouchunying wode

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,13 @@ import "amfe-flexible"
 import 'vant/lib/index.css'
 import '../public/iconfont/iconfont.css'
 import './assets/css/style.css'
+// import './assets/css/mine.css'
+import './assets/image/minefont/iconfont.css'
 import router from './router/index'
 import store from './store/index'
 
 import {
+  Button,
   NavBar,
   Empty,
   Swipe,
@@ -28,8 +31,8 @@ import {
   TabbarItem
 } from "vant";
 
-import {  } from 'vant';
 const app = createApp(App)
+app.use(Button)
 app.use(NavBar)
 app.use(Empty)
 app.use(Swipe)
--- src/views/Mine.vue
+++ src/views/Mine.vue
@@ -1,5 +1,35 @@
 <template>
- <div></div>
+ <div>
+    <div class="top">
+      <img src="../assets/image/mineimg/topbg.png" alt="">
+
+      <div class="nc">
+        <span>遇见温柔风.</span>
+        <a href="">
+          <img src="../assets/image/mineimg/sz.png" alt="">
+        </a>
+      </div>
+
+      <div class="mine">
+        <img src="../assets/image/mineimg/pthy.png" alt="">
+        <span>普通会员</span>
+        <i class="iconfont icon-arrow-right"></i>
+      </div>
+
+      <div class="xyrz">
+        <span>通过信用认证，可享信用免押金</span>
+        <i class="iconfont icon-arrow-right"></i>
+      </div>
+
+      <div class=""></div>
+      
+    </div>
+    
+    
+    
+   
+   <div class="lbt"></div>
+ </div>
 </template>
 
 <script>
@@ -17,4 +47,83 @@ export default {
  methods: {}
 };
 </script>
-<style lang='less' scoped></style>
+<style lang='less' scoped>
+.top {
+  width: 100%;
+  height: 251px;
+  position: relative;
+  img{
+    width: 100%;
+    height: 100%;
+  }
+}
+.nc {
+  width: 319px;
+  height: 20px;
+  position: absolute;
+  top: 42px;
+  padding: 0 28px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  span {
+    color: #fff;
+    font-size: 18px;
+    letter-spacing: 1px;
+  }
+  a{
+    width: 20px;
+    height: 20px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+
+.mine{
+  width: 80px;
+  height: 18px;
+  background: rgba(255, 255, 255,0.3);
+  border: none;
+  border-radius: 9px;
+  position: absolute;
+  top: 80px;
+  left: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  img{
+    margin-left: 8px;
+    width: 12px;
+    height: 15px;
+    margin-right: 2px;
+  }
+  span {
+    font-size: 12px;
+    line-height: 18px;
+  }
+  .icon-arrow-right {
+    font-size: 4px;
+  }
+}
+.xyrz {
+  position: absolute;
+  top: 80px;
+  left: 115px;
+  color: #fff;
+  span {
+    font-size: 11px;
+  }
+  .icon-arrow-right {
+    font-size: 4px;
+  }
+}
+
+.lbt {
+  width: 100%;
+  height: 88px;
+  background:rgb(161, 13, 13);
+}
+</style>
